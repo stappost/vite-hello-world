ce5f76936e3537880fc38327f20408ca2047ed8a
stappost <emanuele.scarpantoni@gmail.com>
2023-12-21 14:43:39 +0100

    Final project

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    data() {
+        return {
+            message: "La mia prima app con Vite!"
+        }
+    },
+}
+</script>
+<template lang="">
+    <div>
+        <h1>{{ message }}</h1>
+    </div>
+</template>
+
+<style lang="">
+    
+</style>
